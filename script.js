16bb6b03adc889b25e6ceb1fb9cbcfea4b7541b8
Uzun Demir <uzunvitt@gmail.com>
2023-03-30 11:48:17 +0300

    Add radio

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+const radioBtns = document.querySelectorAll('input[type=radio]');
+const player = document.createElement("audio");
+const stations = {
+    "r1": "https://icecast.thisisdax.com/SmoothLondonMP3",
+    "r2": "https://icecast.thisisdax.com/CapitalXTRALondonMP3",
+    "r3": "https://online.kissfm.ua/KissFM",
+    "r4": "https://ais-edge102-live365-dal02.cdnstream.com/a80518",
+    "r5": "https://online.radioroks.ua/RadioROKS",
+};
+radioBtns.forEach(rb => {
+    rb.addEventListener('input', e => {
+        const v = rb.value;
+        player.src = stations[v];
+        player.play();
+    });
+});
+///
+const MAGNETIC_DEGS = [0, -45, 45];
+rotateX.oninput = (e) => rotateXHandler();
+rotateY.oninput = (e) => rotateYHandler();
+function rotateXHandler() {
+    const x = magnetic(MAGNETIC_DEGS, parseInt(rotateX.value));
+    rotateX.value = x;
+    front.style.setProperty("--X", x + "deg");
+}
+function rotateYHandler() {
+    const y = magnetic(MAGNETIC_DEGS, parseInt(rotateY.value));
+    rotateY.value = y;
+    front.style.setProperty("--Y", y + "deg");
+}
+function magnetic(array, value, range = 10) {
+    for (let i = 0; i < array.length; i++) {
+        const magnetValue = array[i];
+        const magnetMax = magnetValue + range;
+        const magnetMin = magnetValue - range;
+        if (value < magnetMax && value > magnetMin)
+            return magnetValue;
+    }
+    return value;
+}
