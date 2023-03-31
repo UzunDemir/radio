998b8fedd36ac88db6399e4125b7337914609137
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2023-03-31 17:53:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const stations = {
     "r1": "https://a1.radioheart.ru:9001/gagauzradiosu",
     "r2": "https://str1.pcradio.ru/power_turk-med",
     "r3": "https://str1.pcradio.ru/uzmanfm_com-med",
-    "r4": "https://str1.pcradio.ru/kavkaz_hit-med",
+    "r4": "https://pub0102.101.ru:8443/stream/personal/aacp/64/794981?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJrZXkiOiJlNDQzMWUxMzEyYmJhN2E2ZjdkMmZiOTQ0OGI3MWNhZiIsIklQIjoiOTUuNjUuOTIuMTc2IiwiVUEiOiJNb3ppbGxhXC81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXRcLzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZVwvMTExLjAuMC4wIFNhZmFyaVwvNTM3LjM2IiwiUmVmIjoiaHR0cHM6XC9cLzEwMS5ydVwvIiwidWlkX2NoYW5uZWwiOiI3OTQ5ODEiLCJ0eXBlX2NoYW5uZWwiOiJwZXJzb25hbCIsInR5cGVEZXZpY2UiOiJQQyIsIkJyb3dzZXIiOiJDaHJvbWUiLCJCcm93c2VyVmVyc2lvbiI6IjExMS4wLjAuMCIsIlN5c3RlbSI6IldpbmRvd3MgMTAiLCJleHAiOjE2ODAzMTc0MjN9.PjxkCrFyMR4CI76GVic96QmW-Hs9ls5_sDGsrNCfLm0",
     "r5": "https://str1.pcradio.ru/marusya_fm-med",
 };
 radioBtns.forEach(rb => {
