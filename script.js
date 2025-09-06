0d706a12d970101b691dce7ba2613194a7c3fc51
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2025-09-06 15:52:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const player = document.createElement("audio");
 const stations = {
     "r1": "https://pub0102.101.ru:8443/stream/personal/aacp/64/794981",  
     "r2": "https://old.grt.md/radio",
-    "r3": "https://emg02.hostingradio.ru/retrofmspb128.mp3",
+    "r3": "https://halizgagauz.s02.radio-tochka.com:8025/radio", 
     "r4": "https://a9.radioheart.ru:8010/RH76673",
-    "r5": "https://halizgagauz.s02.radio-tochka.com:8025/radio",
+    "r5": "https://emg02.hostingradio.ru/retrofmspb128.mp3",
 };
 radioBtns.forEach(rb => {
     rb.addEventListener('input', e => {
